c936bd8154b2b3349753d79034c7009bfa89fd3f
LukeMcCann <developer-lukemccann@outlook.com>
2020-11-22 18:07:31 +0000

    Add bootstrap & fontawesome cdn

--- src/app/views/inc/footer.php
+++ src/app/views/inc/footer.php
@@ -1,5 +1,6 @@
 
-
+    </div>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
     <script src="<?php echo URLROOT ?>/js/main.js"></script>
 </body>
 </html>
